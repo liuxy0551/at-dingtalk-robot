221fe968c6dd37c30797da32c4752f897436f7c4
liuyi <liuyi@dtstack.com>
2022-10-20 18:15:40 +0800

    feat: db port

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,10 @@ const dbConfig = {
   port: 3306,
   dialect: 'mysql',
   timezone: '+08:00', // 东八时区
-  database : 'at-dingtalk-xx',
+  database : 'at-dingtalk-robot',
   host : '127.0.0.1',
   user : 'root',
-  password : '1234',
+  password : '123456',
   logging: false
 }
 
